61ff2e7901e8223a4a6d0f3bbfbbb66c237d0277
qa-artem <artem1305@yandex.ru>
2022-12-26 01:08:40 +0300

    refactor tests and build.gradle

--- src/test/java/com/andreev/ozon/WebOzonTests.java
+++ src/test/java/com/andreev/ozon/WebOzonTests.java
@@ -2,12 +2,12 @@ package com.andreev.ozon;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
@@ -15,7 +15,15 @@ import static io.qameta.allure.Allure.step;
 @DisplayName("Параметризованные тесты для сайта Ozon")
 public class WebOzonTests {
 
-    private final String URL_OZON = "https://www.ozon.ru/";
+    private static final String URL_OZON = "https://www.ozon.ru/";
+
+    @BeforeAll
+    public static void beforeTests(){
+        Configuration.browser = "FIREFOX";
+        step("Открываем главную страницу" + URL_OZON, () -> {
+            open(URL_OZON);
+        });
+    }
 
     @ValueSource(strings = {
             "Одежда и обувь",
@@ -24,36 +32,31 @@ public class WebOzonTests {
     })
     @ParameterizedTest(name = "Есть заголовок раздела {0} в меню категорий на странице " + URL_OZON)
     void checkCategoriesInMenuWithValueSource(String searchQuery) {
-        Configuration.startMaximized = true;
-        Configuration.browser = "FIREFOX";
-        step("Открываем главную страницу" + URL_OZON, () -> {
-            open(URL_OZON);
-        });
         step("Есть категория " + searchQuery + " в меню");
         $("ul[data-widget='horizontalMenu']").shouldHave(Condition.text(searchQuery));
     }
 
     @CsvSource({
-            "KZT, ₸",
-            "RUB, ₽",
-            "USD, $",
+            "KZT, ₸"
+//            "RUB, ₽",
+//            "USD, $",
     })
     @ParameterizedTest(name = "Отображается {1} для валюты {0} в карточке товара")
     void checkCurrencyIconInProductCardWithCsvSource(String currency, String icon) {
-        Configuration.startMaximized = true;
-        Configuration.browser = "FIREFOX";
-        step("Открываем главную страницу" + URL_OZON, () -> {
-            open(URL_OZON);
+        step("Выбираем город из РФ", () -> {
+            $("div[data-widget='addressBookBarWeb']").click();
+            $(byText("Изменить")).click();
+            $(byText("Москва")).click();
         });
         step("Кликаем по кнопке выбора валюты", () ->{
             $("div[data-widget='selectedCurrency']").click();
         });
         step("Выбираем валюту " + currency, () ->{
             $("div[role='listbox']").click();
-            $("div[role='listbox']").setValue(currency).pressEnter();
+            $("input[name='filter']").setValue(currency).pressEnter();
         });
         step("Есть " + icon + " в описании товаров", () -> {
-            $("div[role='skuLine']").shouldHave(Condition.text(icon));
+            $("div[data-widget='skuLine']").shouldHave(Condition.text(icon));
         });
     }
 }
